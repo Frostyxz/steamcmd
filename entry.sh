c23acd366ab871a1cd259dde64ad14f7922b5162
Steele <steelecooke@gmail.com>
2020-02-28 18:23:03 +1000

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -2,4 +2,4 @@
 
 ${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir ${STEAMAPPDIR} +app_update ${STEAMAPPID} +quit
 
-${STEAMCMDDIR}/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=${ARKSESSION} -server -log
+${STEAMAPPDIR}/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=${ARKSESSION} -server -log
