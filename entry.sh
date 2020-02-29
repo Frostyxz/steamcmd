d45c1baf5ef0cbab1b29c88c4d3f50cf83d20f82
Steele <steelecooke@gmail.com>
2020-02-29 18:30:13 +1000

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir ${STEAMAPPDIR} +app_update ${STEAMAPPID} +quit
+${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir ${STEAMAPPDIR} +app_update ${STEAMAPPID} +workshop_download_item 731604991 +quit
 
-${STEAMAPPDIR}/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=${ARKSESSION} -server -log
+${STEAMAPPDIR}/ShooterGame/Binaries/Linux/ShooterGameServer Ragnarok?listen?SessionName=${ARKSESSION} -server -log
