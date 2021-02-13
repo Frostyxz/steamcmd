a3456a6f1e313efe409d8a62a883d2702a4f11b4
Steele <steelecooke@gmail.com>
2021-02-13 17:23:32 +1000

    Update v-entry.sh

--- v-entry.sh
+++ v-entry.sh
@@ -2,4 +2,4 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				+quit
-${STEAMAPPDIR}/valheim_server.x86_64 -name "Frosto Test 1" -port 2456 -world "Dedicated" -password "maAgarino23" -public 1
+/home/steam/steamcmd/valheim-dedicated/valheim_server.x86_64 -name "Frosto Test 1" -port 2456 -world "Dedicated" -password "maAgarino23" -public 1
